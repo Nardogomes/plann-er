c440604cabc8b41f80de5d308df1875ec7726473
Ednardo Gomes <ednardo@email.com>
2024-09-02 21:48:52 -0300

    feat: Criação do componente Button

--- frontend/src/pages/trip-details/important-links.tsx
+++ frontend/src/pages/trip-details/important-links.tsx
@@ -1,4 +1,5 @@
 import { Link2, Plus } from "lucide-react";
+import { Button } from "../../components/button";
 
 export function ImportantLinks() {
   return (
@@ -23,10 +24,10 @@ export function ImportantLinks() {
         </div>
       </div>
 
-      <button className='bg-zinc-800 w-full text-zinc-200 rounded-lg px-5 h-11 font-medium flex justify-center items-center gap-2 hover:bg-zinc-700 duration-200'>
+      <Button variant="secondary" size="full">
         <Plus className='size-5' />
         Cadastrar novo link
-      </button>
+      </Button>
     </div>
   )
 }
